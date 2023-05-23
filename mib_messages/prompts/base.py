e4df975e22fd110c0fb4c36438d32e26c23b83d8
Kira1108 <327485253@163.com>
2023-05-23 15:00:37 +0800

    Initial commit

--- mib_messages/prompts/base.py
+++ mib_messages/prompts/base.py
@@ -6,6 +6,9 @@ from mib_messages.prompts.concise import CONCISE_PROMPT
 from mib_messages.prompts.keywords import KEYWORDS_PROMPT
 from mib_messages.prompts.fewshot import create_default_fewshot_template
 
+from abc import ABC, abstractmethod
+
+
 
 TEMPLATES = dict(
     detailed = DETAILED_PROMPT,
@@ -14,8 +17,17 @@ TEMPLATES = dict(
     keywords = KEYWORDS_PROMPT
 )
 
+class BaseMessagePrompt(ABC):
+    
+    @abstractmethod
+    def format(self, message:str) -> str:
+        ...
+        
+    def __call__(self, message:str) -> str:
+        return self.format(message)
+    
 
-class MessagePrompt:
+class MessagePrompt(BaseMessagePrompt):
     """Get a prompt from one of the template names`detailed`, `concise`, `entity`, `keywords`, `fewshot`"""
     
     def __init__(self, template_name:str, examples:list = None):
